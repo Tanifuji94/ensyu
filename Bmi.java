dd13a440814a6e55312aa02bd25eef4b0563e52a
tanifuji <h.tanifuji.sys22@morijyobi.ac.jp>
2022-11-10 10:33:53 +0900

    Bmi

--- Bmi.java
+++ Bmi.java
@@ -4,18 +4,18 @@ public class Main{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("height:");
+		System.out.print("身長1:");
 		double height1 = scanner.nextDouble();
-		System.out.print("weight:");
+		System.out.print("体重1:");
 		double weight1 = scanner.nextDouble();
 
-		System.out.print("height:");
+		System.out.print("身長2:");
 		double height2 = scanner.nextDouble();
-		System.out.print("weight:");
+		System.out.print("身長2:");
 		double weight2 = scanner.nextDouble();
 
-		double bmi1 = weight1 / Math.pow(height1,2);
-		double bmi2 = weight2 / Math.pow(height2,2);
+		double bmi1 = weight1 / (height1 * height1);
+		double bmi2 = weight2 / (height2 * height2);
 
 		System.out.println(bmi1);
 		System.out.println(bmi2);
